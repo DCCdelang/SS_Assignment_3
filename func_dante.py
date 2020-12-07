b70a226de779dc0fdf8b3c4fc8f0c98b2e32fa32
DCCdelang <dccdelang@gmail.com>
2020-12-07 16:31:59 +0100

    runfile

--- func_dante.py
+++ func_dante.py
@@ -1,14 +1,15 @@
 """
-Main file to read out data from the tsp files
+Main file with all functions needed for run.py
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-tsp_file = "TSP-Configurations/eil51.tsp.txt"
-
 def make_matrix(tsp_file):
+    """"
+    Creates an adjacency matrix based on the tsp file
+    """ 
     # Extracting node coordinates from tsp file
     node_list = []
     with open(tsp_file,"r") as reader:
@@ -30,10 +31,16 @@ def make_matrix(tsp_file):
     return adjacency_matrix
 
 # primarly based on https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
-def cost_change(cost_mat, n1, n2, n3, n4):
-    return cost_mat[n1][n3] + cost_mat[n2][n4] - cost_mat[n1][n2] - cost_mat[n3][n4]
+def cost_change(adjacency_matrix, n1, n2, n3, n4):
+    """
+    Calculates change of cost for two_opt function
+    """
+    return adjacency_matrix[n1][n3] + adjacency_matrix[n2][n4] - adjacency_matrix[n1][n2] - adjacency_matrix[n3][n4]
 
-def two_opt(route, cost_mat):
+def two_opt(route, adjacency_matrix):
+    """
+    Calculates the best route using greedy two_opt
+    """
     best = route
     improved = True
     while improved:
@@ -41,7 +48,7 @@ def two_opt(route, cost_mat):
         for i in range(1, len(route) - 2):
             for j in range(i + 1, len(route)):
                 if j - i == 1: continue
-                if cost_change(cost_mat, best[i - 1], best[i], best[j - 1], best[j]) < 0:
+                if cost_change(adjacency_matrix, best[i - 1], best[i], best[j - 1], best[j]) < 0:
                     best[i:j] = best[j - 1:i - 1:-1]
                     improved = True
         route = best
@@ -49,36 +56,38 @@ def two_opt(route, cost_mat):
 
 def calculate_cost(route, adjacency_matrix):
     '''
-    Returns the cost of the current route
+    Returns the cost of the current route based on adjacency matrix
     '''
     route_shifted = np.roll(route,1)
     cost = np.sum(adjacency_matrix[route, route_shifted])
     return cost
 
-def run_two_opt():
+def run_two_opt(tsp_file, N_sim):
+    """
+    Run function for greedy two_opt function
+    """
     best_routes = []
-    len_routes = []
-    N_sim = 300
+    calculate_costs = []
     adjacency_matrix = make_matrix(tsp_file)
 
     for _ in range(N_sim):
         x = list(range(len(adjacency_matrix)))
         init_route = random.sample(x,len(x))
-        # print(init_route)
-        # print(calculate_cost(init_route,adjacency_matrix))
+        print(init_route)
+        print(calculate_cost(init_route,adjacency_matrix))
 
-        adjacency_matrix = make_matrix(tsp_file)
         best_route = two_opt(init_route, adjacency_matrix)
-        # print(best_route)
-        # print(calculate_cost(best_route,adjacency_matrix))
-        len_routes.append(calculate_cost(best_route,adjacency_matrix))
-        best_routes.append(best_routes)
+        print(best_route)
+        print(calculate_cost(best_route,adjacency_matrix))
+        calculate_costs.append(calculate_cost(best_route,adjacency_matrix))
+        best_routes.append(best_route)
 
-    plt.plot(range(N_sim),best_routes)
-    plt.show()
+    return best_routes, calculate_costs
 
- 
-def two_opt_annealing(T, scheme, route, adjacency_matrix):
+def tsp_annealing(T, scheme, route, adjacency_matrix):
+    """
+    Annealing function with different parameter possibilities
+    """
     best = route
     a = 10
     b = 200
@@ -114,7 +123,10 @@ def two_opt_annealing(T, scheme, route, adjacency_matrix):
     print("MC =", MC, "Changes =", changes)
     return best
 
-def run_two_opt_anneal(tsp_file, T, scheme, N_sim):
+def run_annealing(tsp_file, T, scheme, N_sim):
+    """
+    Run function for annealing function
+    """
     best_routes = []
     calculate_costs = []
     adjacency_matrix = make_matrix(tsp_file)
@@ -125,7 +137,7 @@ def run_two_opt_anneal(tsp_file, T, scheme, N_sim):
         print(init_route)
         print(calculate_cost(init_route,adjacency_matrix))
 
-        best_route = two_opt_annealing(T, scheme, init_route, adjacency_matrix)
+        best_route = tsp_annealing(T, scheme, init_route, adjacency_matrix)
 
         print(best_route)
         print(calculate_cost(best_route,adjacency_matrix))
@@ -135,18 +147,21 @@ def run_two_opt_anneal(tsp_file, T, scheme, N_sim):
     return best_routes, calculate_costs
 
 def plot_route(tsp_file,route):
+    """
+    Plot function to show route
+    """
     node_list = []
     with open(tsp_file,"r") as reader:
         for line in reader:
             if line[0].isdigit() == True:
                 node_list.append([int(x) for x in line.split()])
     for i in range(len(node_list)):
-        plt.scatter(node_list[i][1],node_list[i][2])
+        plt.scatter(node_list[i][1],node_list[i][2],c="r")
     for i in range(len(route)-1):
         node1 = node_list[route[i]]
         node2 = node_list[route[i+1]]
-        plt.plot([node1[1],node2[1]],[node1[2],node2[2]])
+        plt.plot([node1[1],node2[1]],[node1[2],node2[2]],"b")
     node1 = node_list[route[-1]]
     node2 = node_list[route[0]]
-    plt.plot([node1[1],node2[1]],[node1[2],node2[2]])
+    plt.plot([node1[1],node2[1]],[node1[2],node2[2]], "b")
     plt.show()
--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 from func_dante import *
 
+
+tsp_file = "TSP-Configurations/a280.tsp.txt"
 T = 500
 scheme = "log" # "lin" or "log"
 N_sim = 1
-routes, costs = run_two_opt_anneal(tsp_file, T, scheme, N_sim)
+
+routes, costs = run_annealing(tsp_file, T, scheme, N_sim)
 route = routes[0]
 print(route)
 plot_route(tsp_file,route)
+plt.show()
+
+routes, costs = run_two_opt(tsp_file, N_sim)
+route = routes[0]
+print(route)
+plot_route(tsp_file,route)
+plt.show()
