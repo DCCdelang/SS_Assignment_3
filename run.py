1ecbba86ef3f1ad87192549de69807b5ce56d09c
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-12-07 17:50:43 +0100

    d

--- run.py
+++ run.py
@@ -3,20 +3,18 @@ from func_dante import *
 
 tsp_file = "TSP-Configurations/eil51.tsp.txt"
 T = 10000
-scheme = "lin" # "lin", "log" or "std"
 N_sim = 1
 
+scheme = "lin" # "lin", "log" or "std"
 routes, costs = run_annealing(tsp_file, T, scheme, N_sim)
 route = routes[0]
 plot_route(tsp_file,route)
-# plt.show()
 print('lin scheme =', costs[-1])
 
 scheme = "log" # "lin", "log" or "std"
 routes, costs = run_annealing(tsp_file, T, scheme, N_sim)
 route = routes[0]
 plot_route(tsp_file,route)
-# plt.show()
 print('log scheme =', costs[-1])
 
 scheme = "std" # "lin", "log" or "std"
