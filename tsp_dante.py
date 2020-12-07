b8929ce13ee9dfa8082d0d3d5fd756bb27d94489
DCCdelang <dccdelang@gmail.com>
2020-12-07 14:35:48 +0100

    additions

--- tsp_dante.py
+++ tsp_dante.py
@@ -29,6 +29,7 @@ def make_matrix(tsp_file):
 
     return adjacency_matrix
 
+# primarly based on https://stackoverflow.com/questions/53275314/2-opt-algorithm-to-solve-the-travelling-salesman-problem-in-python
 def cost_change(cost_mat, n1, n2, n3, n4):
     return cost_mat[n1][n3] + cost_mat[n2][n4] - cost_mat[n1][n2] - cost_mat[n3][n4]
 
@@ -50,6 +51,7 @@ def len_route(route,adjacency_matrix):
     length = 0
     for i in range(len(route)-1):
         length += adjacency_matrix[route[i]][route[i+1]]
+    length += adjacency_matrix[route[-1]][route[0]]
     return length
 
 def run_two_opt():
@@ -74,3 +76,62 @@ def run_two_opt():
     plt.plot(range(N_sim),best_routes)
     plt.show()
 
+def two_opt_annealing(route, adjacency_matrix):
+    best = route
+    T = 500 # initial T
+    a = 10
+    b = 200
+    MC = 0
+    changes = 0
+    while T > 1.35:
+        # T = T*0.99
+        # print(T)
+        T = a/np.log(changes+b)
+
+        # Sample city from route
+        index1, index2 = np.random.randint(0,len(route),size=2)
+        cost0 = len_route(route,adjacency_matrix)
+
+        route[index1], route[index2] = route[index2], route[index1]
+        cost1 = len_route(route,adjacency_matrix)
+
+        if cost1 < cost0:
+            cost0 = cost1
+            changes += 1
+        else:
+            U = np.random.uniform()
+            if U < np.exp((cost0-cost1)/T):
+                # print(T, np.exp((cost0-cost1)/T))
+                cost0 = cost1
+                MC += 1
+                changes += 1
+            else:
+                route[index1], route[index2] = route[index2], route[index1]
+        route = best
+    print("MC =", MC, "Changes =", changes)
+    return best
+
+def run_two_opt_anneal():
+    best_routes = []
+    len_routes = []
+    N_sim = 1
+    adjacency_matrix = make_matrix(tsp_file)
+
+    for _ in range(N_sim):
+        x = list(range(len(adjacency_matrix)))
+        init_route = random.sample(x,len(x))
+        print(init_route)
+        print(len_route(init_route,adjacency_matrix))
+
+        adjacency_matrix = make_matrix(tsp_file)
+        best_route = two_opt_annealing(init_route, adjacency_matrix)
+
+        print(best_route)
+        print(len_route(best_route,adjacency_matrix))
+        len_routes.append(len_route(best_route,adjacency_matrix))
+        best_routes.append(best_routes)
+
+    # plt.plot(range(N_sim),best_routes)
+    # plt.show()
+
+run_two_opt_anneal()
