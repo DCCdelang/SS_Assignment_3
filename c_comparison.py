040c622baf9c029503f80795bc35d457cdbe68a6
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-12-10 11:56:50 +0100

    a

--- c_comparison.py
+++ c_comparison.py
@@ -9,7 +9,7 @@ from plot_route import plot_route
 import pandas as pd
 import time
 
-def calculate_and_plot(cost_lists, N_sim):
+def calculate_and_plot(cost_lists, N_sim, c = '2-opt'):
     '''
     returns mean values for elements of lists in list
     for all different constant values of the cooling scheme
@@ -23,16 +23,15 @@ def calculate_and_plot(cost_lists, N_sim):
             temp_list.append(cost_lists[i][j])
         means.append(np.mean(temp_list))
 
-        # if j == len(cost_lists[0]):
-
 
 
     # put data in pandas df and write to csv
-    df_means = pd.DataFrame(means,dtype=float)
-    df_means.to_csv(f"data/cooling_rate_{c}.csv")
+    if c == '2-opt':
+        df_means = pd.DataFrame(means,dtype=float)
+        df_means.to_csv(f"data/cooling_rate_{c}.csv")
 
     # plot data
-    print('c =', c, ':', means[-1])
+    print('type =', c, ':', means[-1])
     plt.plot(range(len(means)), means, label = f'c = .{c}')
     
 
@@ -49,14 +48,15 @@ scheme = "lin"
 
 constants = [.2, .4, .6, .8, .975]
 
+# plot 2opt costs
+_, _, cost_lists_2opt = run_two_opt(tsp_file, N_sim, max_chain_length)
+calculate_and_plot(cost_lists_2opt, N_sim)
+print('hoi')
+# plot sim annealing costs
 for c in constants:
     _, _, cost_lists = run_two_opt_annealing(tsp_file, T, scheme, N_sim, max_chain_length, c)
-    calculate_and_plot(cost_lists, N_sim)
-
-# # routes, costs = run_two_opt(tsp_file, N_sim)
-# route = routes[0]
-# print('2opt =', costs[-1])
-# plot_route(tsp_file,route)
+    calculate_and_plot(cost_lists, N_sim, c)
+    print('hoi')
 
 t1 = time.time()
 print("The simulation took ", round(t1-t0), 'seconds')
